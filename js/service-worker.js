d1df5f4fc08a502391c6273df9c1f52a30b06ed7
silas97 <silasmaike10@gmail.com>
2024-03-09 18:36:24 -0300

    fix: Corrindo problema do service worker.

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,21 +1,25 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('my-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/style.css',
-        '/js/script.js',
-        '/img/icon.png'
-      ]);
-    })
+      caches.open("my-cache").then((cache) => {
+          return cache.addAll([
+              "./",
+              "./index.html",
+              "./css/style.css",
+              "./js/script.js",
+              "./img/icon.png",
+          ]).catch((error) => {
+              console.error('Failed to add resources to cache:', error);
+          });
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      }).catch((error) => {
+          console.error('Error fetching resource from cache:', error);
+      })
   );
 });
