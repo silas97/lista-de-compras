b07c7d8fc742b2dda8d83a38929a0771f7121b8d
silas97 <silasmaike10@gmail.com>
2024-03-09 18:56:40 -0300

    chore: Ajustando service worker.

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,50 +1,21 @@
-importScripts(
-	"https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
-const CACHE = "pwabuilder-page";
-
-const offlineFallbackPage = "./pages/offline.html";
-
-self.addEventListener("message", (event) => {
-	if (event.data && event.data.type === "SKIP_WAITING") {
-		self.skipWaiting();
-	}
-});
-
-self.addEventListener("install", async (event) => {
+self.addEventListener("install", (event) => {
 	event.waitUntil(
-		caches.open(CACHE).then((cache) => {
-			return cache.addAll([offlineFallbackPage]).catch((error) => {
-				console.error('Failed to add offline page to cache:', error);
-			});
+		caches.open("my-cache").then((cache) => {
+			return cache.addAll([
+				"/",
+				"/index.html",
+				"/css/style.css",
+				"/js/script.js",
+				"/img/icon.png",
+			]);
 		})
 	);
 });
 
-if (workbox.navigationPreload.isSupported()) {
-	workbox.navigationPreload.enable();
-}
-
 self.addEventListener("fetch", (event) => {
-	if (event.request.mode === "navigate") {
-		event.respondWith(
-			(async () => {
-				try {
-					const preloadResp = await event.preloadResponse;
-
-					if (preloadResp) {
-						return preloadResp;
-					}
-
-					const networkResp = await fetch(event.request);
-					return networkResp;
-				} catch (error) {
-					const cache = await caches.open(CACHE);
-					const cachedResp = await cache.match(offlineFallbackPage);
-					return cachedResp;
-				}
-			})()
-		);
-	}
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			return response || fetch(event.request);
+		})
+	);
 });
