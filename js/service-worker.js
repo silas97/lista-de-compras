a309b94646c2aeadb231f9bda60906da0ed4cfe9
silas97 <silasmaike10@gmail.com>
2024-03-09 19:23:17 -0300

    fix: Ajustando PWA.

--- js/service-worker.js
+++ js/service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener("install", (event) => {
 	event.waitUntil(
 		caches.open("my-cache").then((cache) => {
 			return cache.addAll([
-				"/",
-				"/index.html",
-				"/css/style.css",
-				"/js/script.js",
-				"/img/icon.png",
-				"/img/favicon.ico",
+				"/lista-de-compras/",
+				"/lista-de-compras/index.html",
+				"/lista-de-compras/css/style.css",
+				"/lista-de-compras/js/script.js",
+				"/lista-de-compras/img/icon.png",
+				"/lista-de-compras/img/favicon.ico",
 			]);
 		})
 	);
